dc65e8e95775679bc1cd9e9e000322a18e84f4c7
mikechess <mikecook450@gmail.com>
2024-02-11 14:59:41 -0500

    04.Fetching the Blogs

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import { getCategorisedArticles } from '@/lib/articles'
 import React from 'react'
 
 const HomePage = () => {
+  const articles = getCategorisedArticles()
+
+  console.log(articles)
   return (
     <section className="mx-auto w-11/12 md:w-1/2 mt-20 flex flex-col gap-16 mb-20">
       <header className="font-cormorantGaramond font-light text-6xl text-neutral-900 text-center">
